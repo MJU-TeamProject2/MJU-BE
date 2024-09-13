adff4c922df6be1b85786a5a5cc014ab656261b5
woowal <jijile44@gmail.com>
2024-09-13 13:15:42 +0900

    refactor: 클래스 이름의 대소문자를 변경한다

--- src/main/java/com/example/demo/TeamProject2Application.java
+++ src/main/java/com/example/demo/TeamProject2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Teamproject2Application {
+public class TeamProject2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Teamproject2Application.class, args);
+		SpringApplication.run(TeamProject2Application.class, args);
 	}
 
 }
