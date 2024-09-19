6c10dd9d32a8c9c870335e1dc5f59eebcad42a4d
gyuchangShim <shim17@mju.ac.kr>
2024-09-19 15:43:01 +0900

    refactor: baseentity 사용을 위한 어노테이션 추가

--- src/main/java/com/example/demo/common/util/BaseEntity.java
+++ src/main/java/com/example/demo/common/util/BaseEntity.java
@@ -2,12 +2,20 @@ package com.example.demo.common.util;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.EntityListeners;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
+	@CreatedDate
 	private LocalDateTime createdAt;
+	@LastModifiedDate
 	private LocalDateTime updatedAt;
 
 }
