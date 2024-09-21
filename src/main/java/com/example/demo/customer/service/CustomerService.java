f30cf83ad0b593185814e1d4b894b8097bf2ea87
Gyulguma <ybg653924@gmail.com>
2024-09-22 02:46:53 +0900

    feat: CustomerService login 로직 구현

--- src/main/java/com/example/demo/customer/service/CustomerService.java
+++ src/main/java/com/example/demo/customer/service/CustomerService.java
@@ -1,9 +1,16 @@
 package com.example.demo.customer.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.common.exception.CustomException;
+import com.example.demo.common.security.TokenProvider;
+import com.example.demo.customer.dto.response.LoginResponse;
 import com.example.demo.customer.entity.Customer;
+import com.example.demo.customer.entity.CustomerAuth;
 import com.example.demo.customer.repository.CustomerRepository;
+import com.example.demo.exception.CustomerErrorCode;
+import com.example.demo.exception.CustomerNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +18,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CustomerService {
 	private final CustomerRepository customerRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final TokenProvider tokenProvider;
+	private final CustomerAuthService customerAuthService;
 
 	public void register(Customer customer) {
 		customerRepository.save(customer);
@@ -18,5 +28,25 @@ public class CustomerService {
 
 	public boolean checkEmailDuplicate(String email) {
 		return customerRepository.findByEmail(email).isPresent();
+	}
+
+	public LoginResponse login(String email, String password) {
+		Customer customer = customerRepository.findByEmail(email)
+			.orElseThrow(() -> new CustomerNotFoundException());
+		if (!passwordEncoder.matches(password, customer.getPassword())) {
+			throw new CustomException(CustomerErrorCode.CUSTOMER_WRONG_PASSWORD);
+		}
+
+		String accessToken = tokenProvider.createAccessToken(customer.getId());
+		String refreshToken = tokenProvider.createRefreshToken(customer.getId());
+
+		CustomerAuth customerAuth = customerAuthService.findByCustomerId(customer.getId());
+		customerAuth.updateRefreshToken(refreshToken);
+
+		return LoginResponse.builder()
+			.customerId(customer.getId())
+			.accessToken(accessToken)
+			.refreshToken(refreshToken)
+			.build();
 	}
 }
