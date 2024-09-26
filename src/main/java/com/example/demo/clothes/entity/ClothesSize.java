3904d27eb3d38d9fc6c7f6152e115ab5a59b9131
Gyulguma <ybg653924@gmail.com>
2024-09-26 15:33:29 +0900

    feat: ClothesSize 필드 추가

--- src/main/java/com/example/demo/clothes/entity/ClothesSize.java
+++ src/main/java/com/example/demo/clothes/entity/ClothesSize.java
@@ -1,13 +1,17 @@
 package com.example.demo.clothes.entity;
 
-import org.springframework.context.annotation.Primary;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
@@ -21,8 +25,22 @@ public class ClothesSize {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "clothes_size_id")
-	Long clothesSizeId;
+	private Long clothesSizeId;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "clothes_id")
+	private Clothes clothes;
 
+	@Enumerated(EnumType.STRING)
+	private Size size;
 
+	private int quantity;
+
+	@Builder
+	public ClothesSize(Long clothesSizeId, Clothes clothes, Size size, int quantity) {
+		this.clothesSizeId = clothesSizeId;
+		this.clothes = clothes;
+		this.size = size;
+		this.quantity = quantity;
+	}
 }
