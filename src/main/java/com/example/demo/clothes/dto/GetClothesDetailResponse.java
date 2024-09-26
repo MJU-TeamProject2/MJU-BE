b54b92713c415b96b9099f6ca93f98611037d3b9
Gyulguma <ybg653924@gmail.com>
2024-09-26 23:38:10 +0900

    feat: GetClothesDetailResponse 스웨거 설정 및 from 추가

--- src/main/java/com/example/demo/clothes/dto/GetClothesDetailResponse.java
+++ src/main/java/com/example/demo/clothes/dto/GetClothesDetailResponse.java
@@ -2,22 +2,47 @@ package com.example.demo.clothes.dto;
 
 import java.util.List;
 
-import com.example.demo.clothes.entity.ClothesSize;
+import com.example.demo.clothes.entity.Clothes;
 import com.example.demo.clothes.entity.GenderCategory;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-@Schema(name = "Clothes Detail Response", description = "특정 옷 상세 정보 조회 응답")
-public record GetClothesDetailResponse(Long clothesId,
+@Schema(name = "Clothes Detail Response", description = "특정 의류 상세 정보 조회 응답")
+public record GetClothesDetailResponse(@Schema(description = "의류 id", requiredMode = Schema.RequiredMode.REQUIRED)
+									   Long clothesId,
+									   @Schema(description = "의류 카테고리", requiredMode = Schema.RequiredMode.REQUIRED)
 									   String category,
+									   @Schema(description = "의류 이미지", requiredMode = Schema.RequiredMode.REQUIRED)
 									   String imageUrl,
+									   @Schema(description = "의류 이름", requiredMode = Schema.RequiredMode.REQUIRED)
 									   String name,
+									   @Schema(description = "의류 가격", requiredMode = Schema.RequiredMode.REQUIRED)
 									   int price,
+									   @Schema(description = "의류 성별 분류", requiredMode = Schema.RequiredMode.REQUIRED)
 									   GenderCategory genderCategory,
+									   @Schema(description = "의류 품번", requiredMode = Schema.RequiredMode.REQUIRED)
 									   String productNumber,
+									   @Schema(description = "의류 할인률", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 									   int discount,
+									   @Schema(description = "의류 상세설명 이미지(페이지)", requiredMode = Schema.RequiredMode.REQUIRED)
 									   String detailUrl,
-									   List<ClothesSize> clothesSizeList) {
+									   @Schema(description = "의류 사이즈 및 사이즈별 수량 리스트", requiredMode = Schema.RequiredMode.REQUIRED)
+									   List<GetClothesSizeResponse> clothesSizeList) {
+
+	public static GetClothesDetailResponse from(Clothes clothes) {
+		return GetClothesDetailResponse.builder()
+			.clothesId(clothes.getId())
+			.category(clothes.getCategory())
+			.imageUrl(clothes.getImageUrl())
+			.name(clothes.getName())
+			.price(clothes.getPrice())
+			.genderCategory(clothes.getGenderCategory())
+			.productNumber(clothes.getProductNumber())
+			.discount(clothes.getDiscount())
+			.detailUrl(clothes.getDetailUrl())
+			.clothesSizeList(clothes.getClothesSizeList().stream().map(GetClothesSizeResponse::from).toList())
+			.build();
+	}
 }
