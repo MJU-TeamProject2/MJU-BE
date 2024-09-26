f090f769aab52637bd4b5885dcdb77378aa3e439
Gyulguma <ybg653924@gmail.com>
2024-09-26 23:39:27 +0900

    feat: 상품 상세 조회 service 구현

--- src/main/java/com/example/demo/clothes/service/ClothesService.java
+++ src/main/java/com/example/demo/clothes/service/ClothesService.java
@@ -3,9 +3,12 @@ package com.example.demo.clothes.service;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.clothes.dto.GetClothesDetailResponse;
 import com.example.demo.clothes.dto.GetClothesResponse;
+import com.example.demo.clothes.entity.Clothes;
 import com.example.demo.clothes.repository.ClothesRepository;
 import com.example.demo.common.dto.PageResponse;
+import com.example.demo.exception.ClothesNotFoundException;
 import com.example.demo.util.PageUtils;
 
 import lombok.RequiredArgsConstructor;
@@ -17,5 +20,10 @@ public class ClothesService {
 
 	public PageResponse<GetClothesResponse> getAllClothes(PageRequest pageRequest) {
 		return PageUtils.toPageResponse(clothesRepository.findAll(pageRequest)).map(GetClothesResponse::from);
+	}
+
+	public GetClothesDetailResponse getClothesDetail(Long clothesId) {
+		Clothes clothes = clothesRepository.findById(clothesId).orElseThrow(ClothesNotFoundException::new);
+		return GetClothesDetailResponse.from(clothes);
 	}
 }
