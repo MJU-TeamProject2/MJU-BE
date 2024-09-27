775598125ac8477dbeb5966b24f261aac00e28f0
Gyulguma <ybg653924@gmail.com>
2024-09-27 16:35:25 +0900

    fix: 의류로 용어 통일

--- src/main/java/com/example/demo/controller/ClothesController.java
+++ src/main/java/com/example/demo/controller/ClothesController.java
@@ -15,10 +15,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "Clothes API", description = "옷 API")
+@Tag(name = "Clothes API", description = "의류 API")
 public interface ClothesController {
 
-	@Operation(summary = "옷 전체 조회", description = "Page에 맞게 옷 조회")
+	@Operation(summary = "의류 전체 조회", description = "Page에 맞게 의류 조회")
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200"
 			, description = "성공적으로 조회")
@@ -29,7 +29,7 @@ public interface ClothesController {
 		@RequestParam(value = "page", required = false, defaultValue = "0") int page,
 		@RequestParam(name = "sort", required = false, defaultValue = "LATEST") String sortOptions);
 
-	@Operation(summary = "옷 상세 조회", description = "특정 옷 상세 정보 조회")
+	@Operation(summary = "의류 상세 조회", description = "특정 의류 상세 정보 조회")
 	@ApiResponses(value = {
 		@ApiResponse(
 			responseCode = "200",
