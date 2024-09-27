63ef26ea3f8cb16bd12c16684aae425fa1f5c581
Gyulguma <ybg653924@gmail.com>
2024-09-27 16:47:52 +0900

    fix: nonnull 추가

--- src/main/java/com/example/demo/clothes/repository/ClothesRepository.java
+++ src/main/java/com/example/demo/clothes/repository/ClothesRepository.java
@@ -5,11 +5,12 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import com.example.demo.clothes.entity.Clothes;
 
 public interface ClothesRepository extends JpaRepository<Clothes, Long> {
-	Page<Clothes> findAll(Pageable pageable);
+	Page<Clothes> findAll(@NonNull Pageable pageable);
 
-	Optional<Clothes> findById(Long clothesId);
+	Optional<Clothes> findById(@NonNull Long clothesId);
 }
