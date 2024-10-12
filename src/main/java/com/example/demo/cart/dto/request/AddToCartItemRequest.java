f1a820c4c9f02f78f057b6ac3e127c81fd8df7f0
wonholim <kds0034@gmail.com>
2024-10-12 11:01:36 +0900

    chore: Deprecated API 수정

--- src/main/java/com/example/demo/cart/dto/request/AddToCartItemRequest.java
+++ src/main/java/com/example/demo/cart/dto/request/AddToCartItemRequest.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 @Builder
 @Schema(name = "Add To Cart Item Request", description = "장바구니에 상품 추가 요청")
 public record AddToCartItemRequest(
-    @Schema(description = "상품 id", required = true)
+    @Schema(description = "상품 id",  requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull(message = "상품 id는 비어있을 수 없습니다.")
     Long clothesId
 ) {}
