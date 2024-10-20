a8151052daa6468aa8a44206f0033b83606c3beb
Gyulguma <ybg653924@gmail.com>
2024-10-20 21:05:58 +0900

    fix: 이미지 url 생성
    의류 상세 조회, 의류 전체 조회, 의류 카테고리 조회의 응답 값인
    imageUrl, detailUrl, objectUrl을 이미지 url로 수정.

--- src/main/java/com/example/demo/clothes/dto/response/GetClothesDetailResponse.java
+++ src/main/java/com/example/demo/clothes/dto/response/GetClothesDetailResponse.java
@@ -11,42 +11,42 @@ import lombok.Builder;
 
 @Builder
 @Schema(name = "Clothes Detail Response", description = "특정 의류 상세 정보 조회 응답")
-public record GetClothesDetailResponse(@Schema(description = "의류 id", requiredMode = Schema.RequiredMode.REQUIRED)
+public record GetClothesDetailResponse(@Schema(description = "의류 id")
 									   Long clothesId,
-									   @Schema(description = "의류 카테고리", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 카테고리")
 									   ClothesCategory category,
-									   @Schema(description = "의류 이미지", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 이미지")
 									   String imageUrl,
-									   @Schema(description = "의류 이름", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 이름")
 									   String name,
-									   @Schema(description = "의류 가격", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 가격")
 									   Integer price,
-									   @Schema(description = "의류 성별 분류", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 성별 분류")
 									   GenderCategory genderCategory,
-									   @Schema(description = "의류 품번", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 품번")
 									   String productNumber,
-									   @Schema(description = "의류 할인률", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+									   @Schema(description = "의류 할인률")
 									   Integer discount,
-									   @Schema(description = "의류 상세설명 이미지(페이지)", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 상세설명 이미지(페이지)")
 									   String detailUrl,
-									   @Schema(description = "의류 사이즈 및 사이즈별 수량 리스트", requiredMode = Schema.RequiredMode.REQUIRED)
+									   @Schema(description = "의류 사이즈 및 사이즈별 수량 리스트")
 									   List<GetClothesSizeResponse> clothesSizeList,
-									   @Schema(description = "의류 Object 파일", requiredMode = Schema.RequiredMode.REQUIRED)
-									   String url) {
+									   @Schema(description = "의류 Object 파일")
+									   String objectUrl) {
 
-	public static GetClothesDetailResponse from(Clothes clothes, String url) {
+	public static GetClothesDetailResponse of(Clothes clothes, String imageUrl, String detailUrl, String objectUrl) {
 		return GetClothesDetailResponse.builder()
 			.clothesId(clothes.getId())
 			.category(clothes.getCategory())
-			.imageUrl(clothes.getImageUrl())
+			.imageUrl(imageUrl)
 			.name(clothes.getName())
 			.price(clothes.getPrice())
 			.genderCategory(clothes.getGenderCategory())
 			.productNumber(clothes.getProductNumber())
 			.discount(clothes.getDiscount())
-			.detailUrl(clothes.getDetailUrl())
+			.detailUrl(detailUrl)
 			.clothesSizeList(clothes.getClothesSizeList().stream().map(GetClothesSizeResponse::from).toList())
-			.url(url)
+			.objectUrl(objectUrl)
 			.build();
 	}
 }
--- src/main/java/com/example/demo/clothes/dto/response/GetClothesResponse.java
+++ src/main/java/com/example/demo/clothes/dto/response/GetClothesResponse.java
@@ -3,11 +3,35 @@ package com.example.demo.clothes.dto.response;
 import com.example.demo.clothes.entity.Clothes;
 import com.example.demo.clothes.entity.GenderCategory;
 
-public record GetClothesResponse(Long id, String imageUrl, String name, Integer price, GenderCategory genderCategory,
-								 String productNumber, Integer discount) {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
-	public static GetClothesResponse from(Clothes clothes) {
-		return new GetClothesResponse(clothes.getId(), clothes.getImageUrl(), clothes.getName(), clothes.getPrice(),
-			clothes.getGenderCategory(), clothes.getProductNumber(), clothes.getDiscount());
+@Builder
+@Schema(name = "Clothes Response", description = "의류 정보 조회 응답")
+public record GetClothesResponse(@Schema(description = "의류 id")
+								 Long id,
+								 @Schema(description = "의류 이미지")
+								 String imageUrl,
+								 @Schema(description = "의류 이름")
+								 String name,
+								 @Schema(description = "의류 가격")
+								 Integer price,
+								 @Schema(description = "의류 성별 분류")
+								 GenderCategory genderCategory,
+								 @Schema(description = "의류 품번")
+								 String productNumber,
+								 @Schema(description = "의류 할인률")
+								 Integer discount) {
+
+	public static GetClothesResponse of(Clothes clothes, String imageUrl) {
+		return GetClothesResponse.builder()
+			.id(clothes.getId())
+			.imageUrl(imageUrl)
+			.name(clothes.getName())
+			.price(clothes.getPrice())
+			.genderCategory(clothes.getGenderCategory())
+			.productNumber(clothes.getProductNumber())
+			.discount(clothes.getDiscount())
+			.build();
 	}
 }
