ff2f27591b40cb7215cdf818f680d012ce61d441
wonholim <kds0034@gmail.com>
2024-10-21 13:33:57 +0900

    fix: 상품 추가시, 수량을 받도록 수정
    상품 추가시에 옷의 번호와 수량을 같이 보내야합니다.

--- src/main/java/com/example/demo/cart/dto/request/AddToCartItemRequest.java
+++ src/main/java/com/example/demo/cart/dto/request/AddToCartItemRequest.java
@@ -1,6 +1,8 @@
 package com.example.demo.cart.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
@@ -9,5 +11,11 @@ import lombok.Builder;
 public record AddToCartItemRequest(
     @Schema(description = "상품 id",  requiredMode = Schema.RequiredMode.REQUIRED)
     @NotNull(message = "상품 id는 비어있을 수 없습니다.")
-    Long clothesId
+    Long clothesId,
+
+    @Schema(description = "변경할 수량",  requiredMode = Schema.RequiredMode.REQUIRED)
+    @Min(value = 0, message = "수량은 0 이상이어야 합니다.")
+    @Max(value = 99, message = "수량은 99 이하이어야 합니다.")
+    @NotNull(message = "수량은 비어있을 수 없습니다.")
+    Integer quantity
 ) {}
