e9ed46fb28c3e1a5c6c0f8c9e1ab8c48daa52d42
wonholim <kds0034@gmail.com>
2024-10-21 16:03:14 +0900

    chore: 장바구니 상세내역 Response 수정
    카트 넘버, 잔여 개수, 사이즈 추가

--- src/main/java/com/example/demo/cart/dto/response/GetCartResponse.java
+++ src/main/java/com/example/demo/cart/dto/response/GetCartResponse.java
@@ -1,30 +1,37 @@
 package com.example.demo.cart.dto.response;
 
 import com.example.demo.cart.entity.Cart;
+import com.example.demo.clothes.entity.Size;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record GetCartResponse(
+    Long cartId,
     Long clothesId,
     String imageUrl,
     String detailUrl,
     String name,
     Integer quantity,
     Integer price,
-    Integer discount
+    Integer discount,
+    Size size,
+    Integer availableQuantity
 ) {
 
   public static List<GetCartResponse> listOf(List<Cart> carts) {
     return carts.stream()
         .map(cart -> GetCartResponse.builder()
+                .cartId(cart.getId())
                 .clothesId(cart.getId())
                 .imageUrl(cart.getClothes().getImageUrl())
                 .detailUrl(cart.getClothes().getDetailUrl())
                 .name(cart.getClothes().getName())
                 .quantity(cart.getQuantity())
                 .price(cart.getClothes().getPrice())
-                .discount(cart.getClothes().getDiscount()))
+                .discount(cart.getClothes().getDiscount())
+                .size(cart.getClothesSize().getSize())
+                .availableQuantity(cart.getClothesSize().getQuantity()))
         .map(GetCartResponse.GetCartResponseBuilder::build)
         .toList();
   }
