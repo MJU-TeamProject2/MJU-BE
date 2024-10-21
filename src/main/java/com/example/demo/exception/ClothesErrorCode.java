f654992893b21e7f852867574a9fc02d4fce5eba
wonholim <kds0034@gmail.com>
2024-10-21 15:59:08 +0900

    feat: ClothesSize 에러코드 추가

--- src/main/java/com/example/demo/exception/ClothesErrorCode.java
+++ src/main/java/com/example/demo/exception/ClothesErrorCode.java
@@ -9,7 +9,8 @@ public enum ClothesErrorCode implements ErrorCode {
 
 	CLOTHES_NOT_FOUND(404, "CT001", "해당 옷을 찾을 수 없습니다."),
 	CLOTHES_DUPLICATE(405, "CT002", "이미 존재하는 의상입니다."),
-	CLOTHES_SIZE_NOT_FOUND(404, "CT003", "해당 옷 사이즈를 찾을 수 없습니다.");
+	CLOTHES_SIZE_NOT_FOUND(404, "CT003", "해당 옷 사이즈를 찾을 수 없습니다."),
+	CLOTHES_INSUFFICIENT_STOCK(422, "CT004", "상품의 재고가 부족합니다.");
 
 	private final int status;
 	private final String code;
