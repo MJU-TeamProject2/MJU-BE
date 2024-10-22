67c35ee32b75e679d45e478ce78bb5caff03b0d3
wonholim <kds0034@gmail.com>
2024-10-22 23:10:35 +0900

    feat: OrderRepository 구현

--- src/main/java/com/example/demo/order/repository/OrderRepository.java
+++ src/main/java/com/example/demo/order/repository/OrderRepository.java
@@ -1,5 +1,15 @@
 package com.example.demo.order.repository;
 
-public interface OrderRepository {
 
+import com.example.demo.customer.entity.Customer;
+import com.example.demo.order.entity.Order;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+  List<Order> findByCustomer(Customer customer);
+  Optional<Order> findOneByIdAndCustomer(Long orderId, Customer customer);
 }
