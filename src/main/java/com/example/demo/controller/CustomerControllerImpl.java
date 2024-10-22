4b6ab092d43e9181aab885d2c34f26d1c41622c0
wonholim <kds0034@gmail.com>
2024-10-23 00:12:12 +0900

    feat: Payment, Address Controller 구현

--- src/main/java/com/example/demo/controller/CustomerControllerImpl.java
+++ src/main/java/com/example/demo/controller/CustomerControllerImpl.java
@@ -1,9 +1,22 @@
 package com.example.demo.controller;
 
+import com.example.demo.customer.dto.request.AddAddressRequest;
+import com.example.demo.customer.dto.request.AddPaymentRequest;
+import com.example.demo.customer.dto.request.UpdateAddressRequest;
+import com.example.demo.customer.dto.request.UpdatePaymentRequest;
+import com.example.demo.customer.dto.response.GetAddressDetailResponse;
+import com.example.demo.customer.dto.response.GetAddressResponse;
+import com.example.demo.customer.dto.response.GetPaymentDetailResponse;
+import com.example.demo.customer.dto.response.GetPaymentResponse;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,6 +82,86 @@ public class CustomerControllerImpl implements CustomerController {
 	public ResponseEntity<SuccessResponse<Void>> updateProfile(JwtInfo jwtInfo,
 		ProfileUpdateRequest profileUpdateRequest) {
 		customerService.updateProfile(jwtInfo.memberId(), profileUpdateRequest);
+		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@GetMapping("/address")
+	public ResponseEntity<SuccessResponse<List<GetAddressResponse>>> getAddresses(@AuthInfo JwtInfo jwtInfo) {
+		return SuccessResponse.of(
+				customerService.getAddresses(jwtInfo.memberId())
+		).asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@GetMapping("/address/{addressId}")
+	public ResponseEntity<SuccessResponse<GetAddressDetailResponse>> getAddressDetail(@AuthInfo JwtInfo jwtInfo,
+			@PathVariable Long addressId) {
+		return SuccessResponse.of(
+				customerService.getAddressDetail(jwtInfo.memberId())
+		).asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@PostMapping("/address")
+	public ResponseEntity<SuccessResponse<Void>> addAddress(@AuthInfo JwtInfo jwtInfo,
+			@Valid @RequestBody AddAddressRequest addAddressRequest) {
+		customerService.addAddress(jwtInfo.memberId(), addAddressRequest);
+		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@PatchMapping("/address")
+	public ResponseEntity<SuccessResponse<Void>> updateAddress(@AuthInfo JwtInfo jwtInfo,
+			@Valid @RequestBody UpdateAddressRequest updateAddressRequest) {
+		customerService.updateAddress(jwtInfo.memberId(), updateAddressRequest);
+		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@DeleteMapping("/address/{addressId}")
+	public ResponseEntity<SuccessResponse<Void>> deleteAddress(@AuthInfo JwtInfo jwtInfo, @PathVariable Long addressId) {
+		customerService.deleteAddress(jwtInfo.memberId(), addressId);
+		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@GetMapping("/payment")
+	public ResponseEntity<SuccessResponse<List<GetPaymentResponse>>> getPayments(@AuthInfo JwtInfo jwtInfo) {
+		return SuccessResponse.of(
+				customerService.getPayments(jwtInfo.memberId())
+		).asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@GetMapping("/payment/{paymentId}")
+	public ResponseEntity<SuccessResponse<GetPaymentDetailResponse>> getPaymentDetail(@AuthInfo JwtInfo jwtInfo,
+		@PathVariable	Long paymentId) {
+		return SuccessResponse.of(
+				customerService.getPaymentDetail(jwtInfo.memberId(), paymentId)
+		).asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@PostMapping("/payment")
+	public ResponseEntity<SuccessResponse<Void>> addPayment(@AuthInfo JwtInfo jwtInfo,
+			@Valid @RequestBody AddPaymentRequest addPaymentRequest) {
+		customerService.addPayment(jwtInfo.memberId(), addPaymentRequest);
+		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@PatchMapping("/payment")
+	public ResponseEntity<SuccessResponse<Void>> updatePayment(@AuthInfo JwtInfo jwtInfo,
+			@Valid @RequestBody UpdatePaymentRequest updatePaymentRequest) {
+		customerService.updatePayment(jwtInfo.memberId(), updatePaymentRequest);
+		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+	}
+
+	@Override
+	@DeleteMapping("/payment/{paymentId}")
+	public ResponseEntity<SuccessResponse<Void>> deletePayment(@AuthInfo JwtInfo jwtInfo, @PathVariable Long paymentId) {
+		customerService.deletePayment(jwtInfo.memberId(), paymentId);
 		return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
 	}
 
