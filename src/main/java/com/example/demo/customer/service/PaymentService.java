ad24ba6669e373922966599c82ee6506a9f43c9e
wonholim <kds0034@gmail.com>
2024-10-24 02:19:07 +0900

    feat: PaymentService 구현

--- src/main/java/com/example/demo/customer/service/PaymentService.java
+++ src/main/java/com/example/demo/customer/service/PaymentService.java
@@ -1,9 +1,13 @@
 package com.example.demo.customer.service;
 
+import com.example.demo.customer.dto.request.AddPaymentRequest;
+import com.example.demo.customer.dto.request.UpdatePaymentRequest;
+import com.example.demo.customer.entity.Address;
 import com.example.demo.customer.entity.Customer;
 import com.example.demo.customer.entity.Payment;
 import com.example.demo.customer.repository.PaymentRepository;
 import com.example.demo.exception.CustomerPaymentNotFoundExeption;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +26,34 @@ public class PaymentService {
 
   public List<Payment> findByCustomerAndDeletedAtIsNull(Customer customer) {
     return paymentRepository.findByCustomerAndDeletedAtIsNull(customer);
+  }
+
+  public Payment findByIdAndCustomerAndDeletedAtIsNull(Long paymentId, Customer customer) {
+    return paymentRepository.findByIdAndCustomerAndDeletedAtIsNull(paymentId, customer)
+        .orElseThrow(CustomerPaymentNotFoundExeption::new);
+  }
+
+  public void addPayment(Customer customer, AddPaymentRequest addPaymentRequest) {
+    paymentRepository.save(
+        Payment.builder()
+            .customer(customer)
+            .cardNumber(addPaymentRequest.cardNumber())
+            .cardProvider(addPaymentRequest.cardProvider())
+            .expiryDate(addPaymentRequest.expiryDate())
+            .build()
+    );
+  }
+
+  public void updatePayment(Payment payment, UpdatePaymentRequest updatePaymentRequest) {
+    payment.update(
+        updatePaymentRequest.cardNumber(),
+        updatePaymentRequest.cardProvider(),
+        updatePaymentRequest.expiryDate()
+    );
+    paymentRepository.save(payment);
+  }
+
+  public void deletePayment(Payment payment) {
+    paymentRepository.softDelete(payment, LocalDateTime.now());
   }
 }
