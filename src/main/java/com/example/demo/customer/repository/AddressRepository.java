7a8d036ee71d385f9fbde0e77e7eaeac71cf405a
wonholim <kds0034@gmail.com>
2024-10-24 01:34:00 +0900

    feat: Repository 쿼리메서드 추가

--- src/main/java/com/example/demo/customer/repository/AddressRepository.java
+++ src/main/java/com/example/demo/customer/repository/AddressRepository.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
   Optional<Address> findOneByIdAndCustomer(Long addressId, Customer customer);
+  List<Address> findByCustomerAndDeletedAtIsNull(Customer customer);
 }
--- src/main/java/com/example/demo/customer/repository/PaymentRepository.java
+++ src/main/java/com/example/demo/customer/repository/PaymentRepository.java
@@ -6,7 +6,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
   Optional<Payment> findOneByIdAndCustomer(Long addressId, Customer customer);
+  List<Payment> findByCustomerAndDeletedAtIsNull(Customer customer);
 }
