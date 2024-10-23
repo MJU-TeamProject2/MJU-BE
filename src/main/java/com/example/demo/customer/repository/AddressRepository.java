0bad9287818f0d3c3c056e213754557513b7dbe3
wonholim <kds0034@gmail.com>
2024-10-24 02:35:29 +0900

    feat: Repository에 메서드 추가

--- src/main/java/com/example/demo/customer/repository/AddressRepository.java
+++ src/main/java/com/example/demo/customer/repository/AddressRepository.java
@@ -2,8 +2,12 @@ package com.example.demo.customer.repository;
 
 import com.example.demo.customer.entity.Address;
 import com.example.demo.customer.entity.Customer;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +16,9 @@ import java.util.List;
 public interface AddressRepository extends JpaRepository<Address, Long> {
   Optional<Address> findOneByIdAndCustomer(Long addressId, Customer customer);
   List<Address> findByCustomerAndDeletedAtIsNull(Customer customer);
+  Optional<Address> findByIdAndCustomerAndDeletedAtIsNull(Long id, Customer customer);
+
+  @Modifying
+  @Query("UPDATE Address a SET a.deletedAt = :deletedAt WHERE a = :address")
+  void softDelete(@Param("address") Address address, @Param("deletedAt") LocalDateTime deletedAt);
 }
