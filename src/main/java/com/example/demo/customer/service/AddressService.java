1cb623f3e25f6a0eca1b0cbbc221dc71ff42c052
wonholim <kds0034@gmail.com>
2024-10-24 02:03:24 +0900

    feat: AddressService 구현

--- src/main/java/com/example/demo/customer/service/AddressService.java
+++ src/main/java/com/example/demo/customer/service/AddressService.java
@@ -1,9 +1,12 @@
 package com.example.demo.customer.service;
 
+import com.example.demo.customer.dto.request.AddAddressRequest;
+import com.example.demo.customer.dto.request.UpdateAddressRequest;
 import com.example.demo.customer.entity.Address;
 import com.example.demo.customer.entity.Customer;
 import com.example.demo.customer.repository.AddressRepository;
 import com.example.demo.exception.CustomerAddressNotFoundException;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +25,38 @@ public class AddressService {
 
   public List<Address> findByCustomerAndDeletedAtIsNull(Customer customer) {
     return addressRepository.findByCustomerAndDeletedAtIsNull(customer);
+  }
+
+  public Address findByIdAndCustomerAndDeletedAtIsNull(Long addressId, Customer customer) {
+    return addressRepository.findByIdAndCustomerAndDeletedAtIsNull(addressId, customer)
+        .orElseThrow(CustomerAddressNotFoundException::new);
+  }
+
+  public void addAddress(Customer customer, AddAddressRequest addAddressRequest) {
+    addressRepository.save(
+        Address.builder()
+            .customer(customer)
+            .name(addAddressRequest.name())
+            .recipient(addAddressRequest.recipient())
+            .zipCode(addAddressRequest.zipCode())
+            .baseAddress(addAddressRequest.baseAddress())
+            .detailAddress(addAddressRequest.detailAddress())
+            .build()
+    );
+  }
+
+  public void updateAddress(Address address, UpdateAddressRequest updateAddressRequest) {
+    address.update(
+        updateAddressRequest.name(),
+        updateAddressRequest.recipient(),
+        updateAddressRequest.zipCode(),
+        updateAddressRequest.baseAddress(),
+        updateAddressRequest.detailAddress()
+    );
+    addressRepository.save(address);
+  }
+
+  public void deleteAddress(Address address) {
+    addressRepository.softDelete(address, LocalDateTime.now());
   }
 }
