b7cee8f05d4c0765dc617ea4df1e681a54b97d68
gyuchangShim <shim17@mju.ac.kr>
2024-10-24 16:45:29 +0900

    fix: 회원정보 수정시 auth code 반영이 안되는 이슈 수정

--- src/main/java/com/example/demo/customer/service/CustomerService.java
+++ src/main/java/com/example/demo/customer/service/CustomerService.java
@@ -1,34 +1,35 @@
 package com.example.demo.customer.service;
 
-import com.example.demo.customer.dto.request.AddAddressRequest;
-import com.example.demo.customer.dto.request.AddPaymentRequest;
-import com.example.demo.customer.dto.request.UpdateAddressRequest;
-import com.example.demo.customer.dto.request.UpdatePaymentRequest;
-import com.example.demo.customer.dto.response.GetAddressDetailResponse;
-import com.example.demo.customer.dto.response.GetAddressResponse;
-import com.example.demo.customer.dto.response.GetPaymentDetailResponse;
-import com.example.demo.customer.dto.response.GetPaymentResponse;
-import com.example.demo.customer.entity.Address;
-import com.example.demo.customer.entity.Payment;
+import java.util.List;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.common.exception.CustomException;
 import com.example.demo.common.security.TokenProvider;
-import com.example.demo.common.util.auth.AuthService;
-import com.example.demo.customer.dto.request.ProfileUpdateRequest;
-import com.example.demo.customer.dto.response.GetCustomerResponse;
-import com.example.demo.customer.dto.response.LoginResponse;
-import com.example.demo.customer.entity.Customer;
 import com.example.demo.common.util.auth.Auth;
+import com.example.demo.common.util.auth.AuthService;
+import com.example.demo.customer.dto.request.AddAddressRequest;
+import com.example.demo.customer.dto.request.AddPaymentRequest;
+import com.example.demo.customer.dto.request.ProfileUpdateRequest;
+import com.example.demo.customer.dto.request.UpdateAddressRequest;
+import com.example.demo.customer.dto.request.UpdatePaymentRequest;
+import com.example.demo.customer.dto.response.GetAddressDetailResponse;
+import com.example.demo.customer.dto.response.GetAddressResponse;
+import com.example.demo.customer.dto.response.GetCustomerResponse;
+import com.example.demo.customer.dto.response.GetPaymentDetailResponse;
+import com.example.demo.customer.dto.response.GetPaymentResponse;
+import com.example.demo.customer.dto.response.LoginResponse;
+import com.example.demo.customer.entity.Address;
+import com.example.demo.customer.entity.Customer;
+import com.example.demo.customer.entity.Payment;
 import com.example.demo.customer.repository.CustomerRepository;
 import com.example.demo.exception.CustomerAuthNotFoundException;
 import com.example.demo.exception.CustomerErrorCode;
 import com.example.demo.exception.CustomerNotFoundException;
 
 import lombok.RequiredArgsConstructor;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -82,6 +83,7 @@ public class CustomerService {
 	@Transactional
 	public void updateProfile(Long customerId, ProfileUpdateRequest profileUpdateRequest) {
 		Customer customer = customerRepository.getReferenceById(customerId);
+		authService.update(customer.getEmail(), profileUpdateRequest.email());
 		customer.update(profileUpdateRequest.email(), profileUpdateRequest.name(), profileUpdateRequest.nickName(),
 			profileUpdateRequest.age(), profileUpdateRequest.phoneNumber());
 	}
