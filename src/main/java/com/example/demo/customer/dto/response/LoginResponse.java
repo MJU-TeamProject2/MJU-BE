3047dc3bf483ed520fb1cc27b2789ef3d0257d82
gyuchangShim <shim17@mju.ac.kr>
2024-10-24 16:28:56 +0900

    fix: customer login시 token만 반환하도록 변경

--- src/main/java/com/example/demo/customer/dto/response/LoginResponse.java
+++ src/main/java/com/example/demo/customer/dto/response/LoginResponse.java
@@ -1,18 +1,12 @@
 package com.example.demo.customer.dto.response;
 
-import com.example.demo.common.util.auth.Role;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 @Schema(name = "LoginResponse", description = "로그인 응답")
-public record LoginResponse(@Schema(description = "로그인한 유저 id", requiredMode = Schema.RequiredMode.REQUIRED)
-							Long customerId,
-							@Schema(description = "Access Token", requiredMode = Schema.RequiredMode.REQUIRED)
+public record LoginResponse(@Schema(description = "Access Token", requiredMode = Schema.RequiredMode.REQUIRED)
 							String accessToken,
 							@Schema(description = "Refresh Token", requiredMode = Schema.RequiredMode.REQUIRED)
-							String refreshToken,
-							@Schema(description = "Role", requiredMode = Schema.RequiredMode.REQUIRED)
-							Role role) {
+							String refreshToken) {
 }
