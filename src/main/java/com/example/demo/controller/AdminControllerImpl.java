72d3828b6ef23f1570dc32cdbd2d4ba35a88af58
wonholim <kds0034@gmail.com>
2024-10-29 19:17:01 +0900

    chore: pagination 추가

--- src/main/java/com/example/demo/controller/AdminControllerImpl.java
+++ src/main/java/com/example/demo/controller/AdminControllerImpl.java
@@ -6,8 +6,10 @@ import com.example.demo.admin.dto.response.AdminGetOrderResponse;
 import com.example.demo.admin.service.application.AdminOrderApplicationService;
 import com.example.demo.common.security.AuthInfo;
 import com.example.demo.common.security.JwtInfo;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import java.util.List;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.admin.dto.request.AdminLoginRequest;
@@ -43,9 +46,17 @@ public class AdminControllerImpl implements AdminController {
 
 	@Override
 	@GetMapping("/orders")
-	public ResponseEntity<SuccessResponse<List<AdminGetOrderResponse>>> getOrders(@AuthInfo JwtInfo jwtInfo) {
+	public ResponseEntity<SuccessResponse<List<AdminGetOrderResponse>>> getOrders(
+			@AuthInfo JwtInfo jwtInfo,
+			@Parameter(description = "사용자 ID", example = "1")
+			@RequestParam(required = false) Long memberId,
+			@Parameter(description = "페이지 크기", example = "20")
+			@RequestParam(value = "size", required = false, defaultValue = "20") int size,
+			@Parameter(description = "페이지 번호", example = "0")
+			@RequestParam(value = "page", required = false, defaultValue = "0") int page
+	) {
 		return SuccessResponse.of(
-				adminOrderApplicationService.getOrders(jwtInfo.memberId())
+				adminOrderApplicationService.getOrders(jwtInfo.memberId(), memberId, PageRequest.of(page, size))
 		).asHttp(HttpStatus.OK);
 	}
 
