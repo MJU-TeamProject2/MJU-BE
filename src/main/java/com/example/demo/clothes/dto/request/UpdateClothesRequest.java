c2d40638fbccaf91bd194d53038f311bd1837ec4
Yoo Byeong gue <106210750+Gyulguma@users.noreply.github.com>
2024-11-01 15:30:13 +0900

    fix: 상품 수정 request body 설정 수정 (#107)
    required로 적용되던 오류를 수정하였습니다.
    example 값을 제거하여 스웨거에서 수정하지 않는 값이
    보내지지 않게 변경하였습니다.

--- src/main/java/com/example/demo/clothes/dto/request/UpdateClothesRequest.java
+++ src/main/java/com/example/demo/clothes/dto/request/UpdateClothesRequest.java
@@ -13,38 +13,29 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
-@Schema(name = "CreateClothesRequest", description = "상품 수정 요청")
+@Schema(name = "UpdateClothesRequest", description = "상품 수정 요청")
 public record UpdateClothesRequest(
-	@Schema(description = "상품 카테고리", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "SHOES, TOPS, OUTERWEAR, PANTS, DRESSES",
-		implementation = ClothesCategory.class)
+	@Schema(description = "상품 카테고리", requiredMode = Schema.RequiredMode.NOT_REQUIRED, implementation = ClothesCategory.class)
 	@ClotheCategoryValidation.ValidClotheCategory
 	ClothesCategory category,
-	@Schema(description = "상품 이름", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "티셔츠")
+	@Schema(description = "상품 이름", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 	String name,
-	@Schema(description = "상품 가격", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "10000")
+	@Schema(description = "상품 가격", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 	@Min(value = 0, message = "가격은 0 이상이어야 합니다")
 	Integer price,
-	@Schema(description = "성별 카테고리", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "MALE, FEMALE, UNISEX", implementation = GenderCategory.class)
+	@Schema(description = "성별 카테고리", requiredMode = Schema.RequiredMode.NOT_REQUIRED, implementation = GenderCategory.class)
 	@GenderCategoryValidation.ValidGenderCategory
 	GenderCategory genderCategory,
-	@Schema(description = "상품 번호", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "PRODUCT-001")
+	@Schema(description = "상품 번호", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 	String productNumber,
-	@Schema(description = "상품 할인율", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "20")
+	@Schema(description = "상품 할인율", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 	@Min(value = 0, message = "할인율은 0 이상이어야 합니다")
 	@Max(value = 100, message = "할인율은 100 이하여야 합니다")
 	Integer discount,
-	@Schema(description = "상품 사이즈", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "S, M, L, XL, XXL", implementation = Size.class)
+	@Schema(description = "상품 사이즈", requiredMode = Schema.RequiredMode.NOT_REQUIRED, implementation = Size.class)
 	@SizeValidation.ValidSize
 	Size size,
-	@Schema(description = "상품 재고", requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-		example = "100")
+	@Schema(description = "상품 재고", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
 	@Min(value = 0, message = "재고는 0 이상이어야 합니다.")
 	Integer quantity,
 	@Schema(description = "상품 메인 이미지", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
