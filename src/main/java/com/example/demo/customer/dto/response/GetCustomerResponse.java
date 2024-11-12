5ed34d93099a1d2f39f286b7dffe9ab2f1f40af6
wonholim <kds0034@gmail.com>
2024-11-12 14:00:59 +0900

    chore: get profile 할시, 추가

--- src/main/java/com/example/demo/customer/dto/response/GetCustomerResponse.java
+++ src/main/java/com/example/demo/customer/dto/response/GetCustomerResponse.java
@@ -4,9 +4,9 @@ import com.example.demo.customer.entity.Customer;
 import com.example.demo.customer.entity.Gender;
 
 public record GetCustomerResponse(String email, String name, String nickName, int age,
-								  Gender gender, String phoneNumber) {
+								  Gender gender, String phoneNumber, int height, int weight, String bodyType) {
 	public static GetCustomerResponse from(Customer customer) {
 		return new GetCustomerResponse(customer.getEmail(), customer.getName(), customer.getNickName(),
-			customer.getAge(), customer.getGender(), customer.getPhoneNumber());
+			customer.getAge(), customer.getGender(), customer.getPhoneNumber(), customer.getHeight(), customer.getWeight(), customer.getBodyType().name());
 	}
 }
