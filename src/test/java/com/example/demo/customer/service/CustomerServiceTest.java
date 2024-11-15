ddc33594bdfb00c999638e969338bcd4bbdad01d
gyuchangShim <shim17@mju.ac.kr>
2024-11-15 21:13:11 +0900

    refactor: test code setting

---
+++ src/test/java/com/example/demo/customer/service/CustomerServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.customer.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerServiceTest {
+
+	@Test
+	void register() {
+	}
+
+	@Test
+	void checkEmailDuplicate() {
+	}
+
+	@Test
+	void login() {
+	}
+}
